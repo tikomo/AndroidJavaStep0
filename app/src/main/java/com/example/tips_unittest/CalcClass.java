00de0acbbaeacab574eb18c7218edae9d304da3e
tsuyoshi komorita <tikomo@gmail.com>
2020-02-23 15:00:31 +0900

    2st commit

---
+++ app/src/main/java/com/example/tips_unittest/CalcClass.java
@@ -0,0 +1,14 @@
+package com.example.tips_unittest;
+
+public class CalcClass {
+
+    /**
+     *
+     * @param val1 値1
+     * @param val2 値2
+     * @return 値1 + 値2
+     */
+    public int add(Integer val1, Integer val2) {
+        return val1 + val2;
+    }
+}
--- app/src/test/java/com/example/sample/ExampleUnitTest.java
+++ app/src/test/java/com/example/sample/ExampleUnitTest.java
@@ -1,5 +1,10 @@
 package com.example.sample;
 
+import android.util.Log;
+
+import com.example.tips.TipsClass;
+import com.example.tips_unittest.CalcClass;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,8 +15,37 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    /**
+     * デバッグログ用のTAG
+     */
+    private final String TAG_NAME = "MY_TAG";
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void test_func1() {
+        CalcClass calcClass = new CalcClass();
+        assertEquals(4, calcClass.add(2,2));
+    }
+
+    @Test
+    public void test_TipsClass() {
+        // ここに作成したクラスのUnitテストを記述する
+        assertEquals(4, 2 + 2);
+    }
+
+    /**
+     * デバッグメッセージを出力する
+     *
+     * @param msg メッセージ
+     */
+    private void debugMsg(String msg) {
+
+        Log.d(TAG_NAME, msg);
+
     }
 }
